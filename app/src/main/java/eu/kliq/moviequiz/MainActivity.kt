e85d7eefe987ec06f6d1b152fae63127e4da7477
kliqkliq <kliqkliq@gmail.com>
2017-09-26 17:01:50 +0200

    Add Question Manager

--- app/src/main/java/eu/kliq/moviequiz/MainActivity.kt
+++ app/src/main/java/eu/kliq/moviequiz/MainActivity.kt
@@ -2,7 +2,6 @@ package eu.kliq.moviequiz
 
 import android.content.Context
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.graphics.PorterDuff
 import android.net.ConnectivityManager
 import android.support.v7.app.AppCompatActivity
@@ -13,47 +12,18 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.RelativeLayout
 
-import java.io.IOException
-import java.net.URL
 import java.util.ArrayList
-import java.util.HashMap
-import java.util.Random
 
-import info.movito.themoviedbapi.TmdbApi
-import info.movito.themoviedbapi.TmdbMovies
-import info.movito.themoviedbapi.model.MovieDb
-import info.movito.themoviedbapi.model.config.TmdbConfiguration
-import info.movito.themoviedbapi.model.core.MovieResultsPage
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
+class MainActivity : AppCompatActivity() {
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    // TheMovieDB API objects
-    private lateinit var mTmdbConfiguration: TmdbConfiguration
-    private lateinit var mTmdbMovies: TmdbMovies
-
+    private val TAG = "MainActivity"
     private var isWaiting = true
-    private var mCurrentPage = 0
-    private var mCurrentQuestion = 0
-    private var mCurrentMovieId = 0
-    private var mCorrectAnswer = 0
-    private lateinit var mCurrentImage: Bitmap
-    private var mCurrentMovies: MutableMap<Int, Int> = HashMap()
-    private lateinit var mBaseUrl: String
-    private lateinit var mMovies: Map<Int, MovieDb>
-    private var mRandom = Random()
-
-    // UI
     private lateinit var mImageView: ImageView
     private lateinit var mButtons: MutableList<Button>
     private lateinit var mWaitingLayout: RelativeLayout
-
-    private val TAG = "MainActivity"
-    private val API_KEY = BuildConfig.TMDB_API_KEY
-    private val BACKDROP_SIZE = "w1280"
     private val CORRECT_ANSWER_COLOR = 0xFF00FF00.toInt()
     private val WRONG_ANSWER_COLOR = 0xFFFF0000.toInt()
+    private val mQuestionManager: QuestionManager = QuestionManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -66,116 +36,49 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         mButtons.add(findViewById(R.id.answer_button3) as Button)
         mButtons.add(findViewById(R.id.answer_button4) as Button)
 
-        for (button in mButtons) {
-            button.setOnClickListener(this)
+        for ((index, button) in mButtons.withIndex()) {
+            button.setOnClickListener({
+                if (!isWaiting) selectAnswer(index)
+            })
         }
 
         val connMgr = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val networkInfo = connMgr.activeNetworkInfo
         if (networkInfo != null && networkInfo.isConnected) {
-            initialize()
+            mQuestionManager.initialize {generateQuestion()}
         }
     }
 
-    private fun initialize() {
-        doAsync {
-            mTmdbMovies = TmdbApi(API_KEY).movies
-            mTmdbConfiguration = TmdbApi(API_KEY).configuration
-            mBaseUrl = mTmdbConfiguration.baseUrl
-            val resultsPage: MovieResultsPage = mTmdbMovies.getPopularMovieList("pl", mCurrentPage++)
-            mMovies = resultsPage.results.associateBy({it.id}, {it})
-            uiThread {
-                generateQuestion()
-            }
-        }
-    }
-
-    private fun generateQuestion() {
-        setWaitingState(true)
-        mCurrentMovies.clear()
-        mCurrentMovieId = randomMovieId
-        getMovieImage(mCurrentMovieId)
-        mCorrectAnswer = mRandom.nextInt(3)
-        for (iteration in 0..3) {
-            // If this iteration is the same as the correct answer, set the ID of the current movie
-            val id = if (iteration == mCorrectAnswer) mCurrentMovieId else uniqueRandomMovieId
-            mCurrentMovies.put(iteration, id)
-        }
-        mCurrentQuestion++
-    }
-
-    private val uniqueRandomMovieId: Int
-        get() {
-            var id: Int
-            do {
-                id = randomMovieId
-            } while (mCurrentMovies.containsValue(id))
-            return id
-        }
-
-    private val randomMovieId: Int
-        get() {
-            val randomEntry = mRandom.nextInt(mMovies.size - 1)
-            return mMovies.keys.toTypedArray()[randomEntry]
-        }
-
-    private fun setUiForCurrentQuestion() {
+    private fun setUiForCurrentQuestion(bitmap: Bitmap) {
         Log.d(TAG, "setUiForCurrentQuestion()")
         // Set image
-        mImageView.setImageBitmap(mCurrentImage)
+        mImageView.setImageBitmap(bitmap)
         // Set buttons
-        for (iteration in 0..3) {
-            val id = mCurrentMovies[iteration]
-            val title = mMovies[id]?.title
+        for (iteration in 0 until QuestionManager.ANSWERS) {
             val button = mButtons[iteration]
-            button.text = title
+            button.text = mQuestionManager.getCurrentQuestionMovieTitle(iteration)
             button.background.colorFilter = null
         }
         setWaitingState(false)
     }
 
-    private fun getMovieImage(id: Int) {
-        Log.d(TAG, "getMovieImage() id: " + id)
-        doAsync {
-            val paths = mTmdbMovies.getImages(id, "").backdrops
-                    .filter { it.language == null }
-                    .mapTo(ArrayList<String>()) { it.filePath }
-            val randomPathId = mRandom.nextInt(paths.size - 1)
-            try {
-                val url = URL(mBaseUrl + BACKDROP_SIZE + paths[randomPathId])
-                mCurrentImage = BitmapFactory.decodeStream(url.openConnection().getInputStream())
-            } catch (e: IOException) {
-                throw RuntimeException(e)
-            }
-            uiThread {
-                setUiForCurrentQuestion()
-            }
-        }
-    }
-
-    override fun onClick(v: View) {
-        if (!isWaiting) {
-            when (v.id) {
-                R.id.answer_button1 -> selectAnswer(0)
-                R.id.answer_button2 -> selectAnswer(1)
-                R.id.answer_button3 -> selectAnswer(2)
-                R.id.answer_button4 -> selectAnswer(3)
-            }
-        }
-    }
-
     private fun selectAnswer(answerId: Int) {
-        Log.d(TAG, "selectAnswer() answerId: " + answerId)
+        Log.d(TAG, "selectAnswer($answerId)")
         val color: Int
-        if (answerId == mCorrectAnswer) {
-            Log.d(TAG, "selectAnswer() true")
+        if (answerId == mQuestionManager.correctAnswer) {
+            Log.d(TAG, "correct answer")
             color = CORRECT_ANSWER_COLOR
         } else {
-            Log.d(TAG, "selectAnswer() false")
+            Log.d(TAG, "wrong answer")
             color = WRONG_ANSWER_COLOR
         }
         mButtons[answerId].background.setColorFilter(color, PorterDuff.Mode.MULTIPLY)
         generateQuestion()
+    }
+
+    private fun generateQuestion() {
+        setWaitingState(true)
+        mQuestionManager.generateQuestion { bitmap -> setUiForCurrentQuestion(bitmap) }
     }
 
     private fun setWaitingState(state: Boolean) {
---
+++ app/src/main/java/eu/kliq/moviequiz/QuestionManager.kt
@@ -0,0 +1,102 @@
+package eu.kliq.moviequiz
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
+import info.movito.themoviedbapi.TmdbApi
+import info.movito.themoviedbapi.TmdbMovies
+import info.movito.themoviedbapi.model.MovieDb
+import info.movito.themoviedbapi.model.config.TmdbConfiguration
+import info.movito.themoviedbapi.model.core.MovieResultsPage
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.io.IOException
+import java.net.URL
+import java.util.*
+
+object QuestionManager {
+    private val TAG = "QuestionManager"
+    private lateinit var mTmdbConfiguration: TmdbConfiguration
+    private lateinit var mTmdbMovies: TmdbMovies
+    private val API_KEY = BuildConfig.TMDB_API_KEY
+    private var mRandom = Random()
+    private var mCurrentPage = 0
+    private var mCurrentQuestion = 0
+    private var mCurrentMovieId = 0
+    private var mCurrentMovies: MutableMap<Int, Int> = HashMap()
+    private lateinit var mBaseUrl: String
+    private lateinit var mMovies: Map<Int, MovieDb>
+    private val BACKDROP_SIZE = "w1280"
+
+    var correctAnswer = 0
+    val ANSWERS = 4
+
+    fun initialize(callback: () -> Unit) {
+        doAsync {
+            mTmdbMovies = TmdbApi(API_KEY).movies
+            mTmdbConfiguration = TmdbApi(API_KEY).configuration
+            mBaseUrl = mTmdbConfiguration.baseUrl
+            val resultsPage: MovieResultsPage = mTmdbMovies.getPopularMovieList("pl", mCurrentPage++)
+            mMovies = resultsPage.results.associateBy({it.id}, {it})
+            uiThread {
+                callback()
+            }
+        }
+    }
+
+    fun generateQuestion(callback: (Bitmap) -> Unit) {
+        Log.d(TAG, "generateQuestion()")
+        mCurrentMovies.clear()
+        mCurrentMovieId = randomMovieId
+        correctAnswer = mRandom.nextInt(ANSWERS - 1)
+        for (iteration in 0 until ANSWERS) {
+            // If this iteration is the same as the correct answer, set the ID of the current movie
+            val id = if (iteration == correctAnswer) mCurrentMovieId else uniqueRandomMovieId
+            Log.d(TAG, "Add answer - button index: $iteration, movieId: $id, correct: " + (iteration == correctAnswer))
+            mCurrentMovies.put(iteration, id)
+        }
+        mCurrentQuestion++
+        doAsync {
+            val currentImage = getMovieImage(mCurrentMovieId)
+            uiThread {
+                callback(currentImage)
+            }
+        }
+    }
+
+    private val uniqueRandomMovieId: Int
+        get() {
+            var id: Int
+            do {
+                id = randomMovieId
+            } while (mCurrentMovieId == id || mCurrentMovies.containsValue(id))
+            return id
+        }
+
+    private val randomMovieId: Int
+        get() {
+            val randomEntry = mRandom.nextInt(mMovies.size - 1)
+            return mMovies.keys.toTypedArray()[randomEntry]
+        }
+
+    fun getCurrentQuestionMovieTitle(number: Int): String? {
+        val id = mCurrentMovies[number]
+        return mMovies[id]?.title
+    }
+
+    private fun getMovieImage(id: Int): Bitmap {
+        Log.d(TAG, "getMovieImage($id)")
+        val currentImage: Bitmap
+        val paths = mTmdbMovies.getImages(id, "").backdrops
+                .filter { it.language == null }
+                .mapTo(ArrayList<String>()) { it.filePath }
+        val randomPathId = mRandom.nextInt(paths.size - 1)
+        try {
+            val url = URL(mBaseUrl + BACKDROP_SIZE + paths[randomPathId])
+            currentImage = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+        } catch (e: IOException) {
+            throw RuntimeException(e)
+        }
+        return currentImage
+    }
+}
